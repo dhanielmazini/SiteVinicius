238a6677760ffd8d41ec179004229978649bf971
Dhaniel Mazini <dnmazini@gmail.com>
2018-03-10 10:30:17 -0300

    Adição das paginas de empreendimento e obras.

--- js/js-index.js
+++ js/js-index.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
     $('body').scrollspy({target: ".navbar", offset: 50});
 
     $("#navbarToggler a").on('click', function(event) {
-    if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-        scrollTop: $(hash).offset().top
-        }, 800, function(){
-        window.location.hash = hash;
-        });
-    }
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1200, function(){
+                window.location.hash = hash;
+            });
+            return false;
+        }
     });
 });
